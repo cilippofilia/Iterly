5fb473ad54c4f66557d98a8fe26f4b3322415ecf
Filippo Cilia <filippocarlocilia@gmail.com>
2026-02-25 18:09:44 +0000

    Initial Commit

---
+++ Itero/IteroApp.swift
@@ -0,0 +1,17 @@
+//
+//  IteroApp.swift
+//  Itero
+//
+//  Created by Filippo Cilia on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct IteroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
